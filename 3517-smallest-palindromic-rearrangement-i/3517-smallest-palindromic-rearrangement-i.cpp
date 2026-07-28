77a76807b84a533d59c02b72b71ff435f7d1a011
Himanshu202401063 <gohelhimanshu07@gmail.com>
2026-07-28 11:39:13 +0530

    Time: 163 ms (14.06%), Space: 81.8 MB (7.91%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -4,18 +4,18 @@ public:
           int n = s.size();
         map<char,int>mp;
         string ans ="";
-        string mid ="";
+        string a="";
           for(auto&i:s)mp[i]++;
         for(auto&i:mp){
          char ch = i.first;
          int frq = i.second;
-               ans += string(frq/2,ch);
-               if(frq%2) mid =ch;
+            ans += string(frq/2,ch);
+            if(frq%2) a+= ch;
         }
          string you = ans;
          reverse(you.begin(),you.end());
        
-         return ans +mid+you;
+         return ans +a+you;
 
     }
 };
